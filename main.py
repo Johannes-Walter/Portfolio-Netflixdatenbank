56c95e43b986ecec2a1147a29c4f856f08e11207
Johannes-Walter <jo.walter97@gmx.de>
2021-06-25 16:10:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 # -*- coding: utf-8 -*-
 import csv
 
-data = []
-
-# In UTF-8 öffnen, weil Sonderzeichen
-with open("netflix_titles.csv", encoding="UTF-8") as file:
-    reader = csv.reader(file)
-    for row in reader:
-        # Zeilenumbrüche Löschen, um aufzuräumen
-        for iter, item in enumerate(row):
-            row[iter] = item.strip("\n")
-        data.append(row)
-
 
 
 """
@@ -23,7 +12,7 @@ with open("netflix_titles.csv", encoding="UTF-8") as file:
     - speichern
     - Suchmaske
     - Rückgabetyp (Pandas?)
-    
+
 - Kartendarstellung (Streamlit/eher Excel?)
 
 - UI mit Streamlit/Flask
@@ -34,7 +23,7 @@ with open("netflix_titles.csv", encoding="UTF-8") as file:
 
 - Video aufnehmen/schneiden
     - 4MAT
-    
+
 
 WAS SOLL MAN DARSTELLEN KÖNNEN?
 WAS DAVON BRINGT ETWAS?
@@ -45,7 +34,7 @@ WAS DAVON BRINGT ETWAS?
 - Wie lang sind Filme aus USA im Schnitt im Gegensatz zu Indien? (Boxplot mit min,avg,max)
 - Wie viel Schauspieler gibt es im Durchschnitt in einer Serie?
 
-    
+
 DV:
 - Dashboard mit matplotlib/streamlit/excel
 
