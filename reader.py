de222ce37eb149b90891fd1209e96cfd28b3ca8b
Johannes-Walter <jo.walter97@gmx.de>
2021-06-25 16:10:02 +0200

    Create reader.py

---
+++ reader.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import sqlite3
+
+
+
+
+class db_connector:
+
+    def __init__(self, db_path):
+        self.con = sqlite3.connect(db_path)
+
+    def __del__(self):
+        self.con.close()
+
+    def read_file(self, file_path: str):
+        # In UTF-8 öffnen, weil Sonderzeichen
+        cur = self.con.cursor()
+        T = True
+        F = False
+
+        data = pd.read_csv(file_path, encoding="UTF-8")
+
+        # Zeilenumbrüche Löschen, um aufzuräumen
+        data = data.replace("\\n", " ", regex=True)
+
+        cur.executemany(
+            """INSERT INTO [shows] (show_id, type, title, date_added, release_year, rating, duration, description)
+            values(?, ?, ?, ?, ?, ?, ?, ?)""",
+            data.loc[:, ["show_id", "type", "title", "date_added", "release_year", "rating", "duration", "description"]].values.tolist())
+
+        self.con.commit()
+        return
+
+    def reset_database(self):
+        cur = self.con.cursor()
+        cur.execute("DROP TABLE IF EXISTS [shows]")
+        cur.execute("""CREATE TABLE [shows] (
+            show_id STRING PRIMARY KEY,
+            type STRNG,
+            title STRING,
+            date_added STRING,
+            release_year INT,
+            rating STRING,
+            duration STRING,
+            description STRING)""")
+        self.con.commit()
+
+
+if __name__ == "__main__":
+    #test = data.loc[:,["show_id", "type", "title", "date_added", "release_year", "rating", "duration", "description"]].values.tolist()
+    con = db_connector("database")
+    con.reset_database()
+    con.read_file("netflix_titles.csv")
+    con.con.close()
