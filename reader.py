3c4115936debad0bdbaaf04f3af225c5f9fb6ea4
Johannes-Walter <jo.walter97@gmx.de>
2021-06-26 18:12:23 +0200

    Update reader.py

--- reader.py
+++ reader.py
@@ -245,7 +245,16 @@ class db_connector:
         return pd.DataFrame(cur.fetchall(), columns=["country", "count"])
 
     def get_countries_by_show(self, show: str):
-        pass
+        cur = self.con.cursor()
+        cur.execute("""
+                    SELECT c.name
+                    FROM [shows] as s, [country] as c, [show_country] as sc
+                    WHERE s.show_id = sc.show_id
+                    AND c.id = sc.country_id
+                    AND s.title = ?
+                    """,
+                    (show,))
+        return pd.DataFrame(cur.fetchall(), columns=("country",))
 
     def get_directors_by_show(self, show: str):
         cur = self.con.cursor()
@@ -260,26 +269,133 @@ class db_connector:
         return pd.DataFrame(cur.fetchall(), columns=("director",))
 
     def get_cast_by_show(self, show: str):
-        pass
+        cur = self.con.cursor()
+        cur.execute("""
+                    SELECT c.name
+                    FROM [shows] as s, [cast] as c, [show_cast] as sc
+                    WHERE s.show_id = sc.show_id
+                    AND c.id = sc.cast_id
+                    AND s.title = ?
+                    """,
+                    (show,))
+        return pd.DataFrame(cur.fetchall(), columns=("cast",))
 
     def get_listings_by_show(self, show: str):
-        pass
+        cur = self.con.cursor()
+        cur.execute("""
+                    SELECT l.name
+                    FROM [shows] as s, [listing] as l, [show_listing] as sl
+                    WHERE s.show_id = sl.show_id
+                    AND l.id = sl.listing_id
+                    AND s.title = ?
+                    """,
+                    (show,))
+        return pd.DataFrame(cur.fetchall(), columns=("listings",))
 
     def get_listings_by_director(self, director: str):
-        pass
+        cur = self.con.cursor()
+        cur.execute("""
+                    SELECT DISTINCT l.name
+                    FROM [listing] as l, [show_listing] as sl,
+                         [director] as d, [show_director] as sd
+                    WHERE sl.show_id = sd.show_id
+                    AND l.id = sl.listing_id
+                    AND d.id = sd.director_id
+                    AND d.name = ?
+                    """,
+                    (director,))
+        return pd.DataFrame(cur.fetchall(), columns=("listing",))
 
     def get_listings_by_cast(self, cast: str):
-        pass
+        cur = self.con.cursor()
+        cur.execute("""
+                    SELECT DISTINCT l.name
+                    FROM [listing] as l, [show_listing] as sl,
+                         [cast] as c, [show_cast] as sc
+                    WHERE sl.show_id = sc.show_id
+                    AND l.id = sl.listing_id
+                    AND c.id = sc.cast_id
+                    AND c.name = ?
+                    """,
+                    (cast,))
+        return pd.DataFrame(cur.fetchall(), columns=("listing",))
 
     def get_cast_by_director(self, director: str):
-        pass
+        cur = self.con.cursor()
+        cur.execute("""
+                    SELECT DISTINCT c.name
+                    FROM [cast] as c, [show_cast] as sc,
+                         [director] as d, [show_director] as sd
+                    WHERE sc.show_id = sd.show_id
+                    AND c.id = sc.cast_id
+                    AND d.id = sd.director_id
+                    AND d.name = ?
+                    """,
+                    (director,))
+        return pd.DataFrame(cur.fetchall(), columns=("cast",))
 
-    def get_directors_by_cast(self, director: str):
+    def get_directors_by_cast(self, cast: str):
+        cur = self.con.cursor()
+        cur.execute("""
+                    SELECT DISTINCT d.name
+                    FROM [cast] as c, [show_cast] as sc,
+                         [director] as d, [show_director] as sd
+                    WHERE sc.show_id = sd.show_id
+                    AND c.id = sc.cast_id
+                    AND d.id = sd.director_id
+                    AND c.name = ?
+                    """,
+                    (cast,))
+        return pd.DataFrame(cur.fetchall(), columns=("director",))
+
+    def get_country_by_director(self, director: str):
+        cur = self.con.cursor()
+        cur.execute("""
+                    SELECT DISTINCT c.name
+                    FROM [country] as c, [show_country] as sc,
+                         [director] as d, [show_director] as sd
+                    WHERE sc.show_id = sd.show_id
+                    AND c.id = sc.country_id
+                    AND d.id = sd.director_id
+                    AND d.name = ?
+                    """,
+                    (director,))
+        return pd.DataFrame(cur.fetchall(), columns=("country",))
+
+    def get_director_by_country(self, country: str):
+        cur = self.con.cursor()
+        cur.execute("""
+                    SELECT DISTINCT d.name
+                    FROM [country] as c, [show_country] as sc,
+                         [director] as d, [show_director] as sd
+                    WHERE sc.show_id = sd.show_id
+                    AND c.id = sc.country_id
+                    AND d.id = sd.director_id
+                    AND c.name = ?
+                    """,
+                    (country,))
+        return pd.DataFrame(cur.fetchall(), columns=("country",))
+
+    def get_country_by_cast(self, cast: str):
+        cur = self.con.cursor()
+        cur.execute("""
+                    SELECT DISTINCT c.name
+                    FROM [country] as c, [show_country] as sc,
+                         [cast] as cast, [show_cast] as scast
+                    WHERE sc.show_id = scast.cast_id
+                    AND c.id = sc.country_id
+                    AND cast.id = scast.cast_id
+                    AND cast.name = ?
+                    """,
+                    (cast,))
+        return pd.DataFrame(cur.fetchall(), columns=("country",))
+
+    def get_cast_by_country(self, country: str):
         pass
 
 if __name__ == "__main__":
     con = db_connector()
     # con.reset_database()
     # con.import_file("netflix_titles.csv")
-    data = con.get_shows_per_country()
+    data = con.get_country_by_cast("Samuel West")
     con.con.close()
