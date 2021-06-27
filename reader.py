02890253c7249278c53e885fd5df2b277569aead
Johannes-Walter <jo.walter97@gmx.de>
2021-06-27 14:04:44 +0200

    Update reader.py

--- reader.py
+++ reader.py
@@ -14,7 +14,7 @@ class db_connector:
     SHOW_COLUMNS = ["show_id", "type", "title", "date_added", "release_year", "rating", "duration", "description"]
     DIRECTOR_COLUMNS = ["id", "name"]
     COUNTRY_COLUMNS = ["id", "name"]
-    LISTING_COLUMNS = ["id", "name"]
+    listed_in_COLUMNS = ["id", "name"]
     CAST_COLUMNS = ["id", "name"]
 
     def __init__(self, db_path = "database"):
@@ -35,7 +35,7 @@ class db_connector:
         data["show_id"] = data["show_id"].replace("s", "", regex=True).astype(int)
 
         cur.executemany(f"""
-            INSERT INTO [shows] ({", ".join(self.SHOW_COLUMNS)})
+            INSERT INTO [show] ({", ".join(self.SHOW_COLUMNS)})
             values(?, ?, ?, ?, ?, ?, ?, ?)""",
             data.loc[:, self.SHOW_COLUMNS].values.tolist())
 
@@ -50,7 +50,7 @@ class db_connector:
                                          data_name="country",
                                          data=row["country"])
             self.__insert_relational_row(show_id=row["show_id"],
-                                         data_name="listing",
+                                         data_name="listed_in",
                                          data=row["listed_in"])
 
         self.con.commit()
@@ -77,9 +77,9 @@ class db_connector:
 
     def reset_database(self):
         cur = self.con.cursor()
-        cur.execute("DROP TABLE IF EXISTS [shows]")
+        cur.execute("DROP TABLE IF EXISTS [show]")
         cur.execute("""
-            CREATE TABLE [shows] (
+            CREATE TABLE [show] (
             show_id INTEGER PRIMARY KEY,
             type STRING,
             title STRING,
@@ -89,7 +89,7 @@ class db_connector:
             duration STRING,
             description STRING)""")
 
-        for table in ("country", "cast", "director", "listing"):
+        for table in ("country", "cast", "director", "listed_in"):
             cur.execute(f"DROP TABLE IF EXISTS [{table}]")
             cur.execute(f"""
                 CREATE TABLE [{table}](
@@ -120,7 +120,7 @@ class db_connector:
             cur.execute(f"""
                 CREATE VIEW [show_per_{table}] AS
                     SELECT s.{", s.".join(self.SHOW_COLUMNS)}, tbl.name AS [{table}]
-                    FROM [shows] AS s
+                    FROM [show] AS s
                     LEFT JOIN [show_{table}] AS show_table
                         ON s.show_id = show_table.show_id
                     LEFT JOIN [{table}] as tbl
@@ -129,7 +129,7 @@ class db_connector:
                     AND tbl.name NOT NULL
                 """)
 
-        for table1, table2 in itertools.permutations(("country", "cast", "director", "listing"), 2):
+        for table1, table2 in itertools.permutations(("country", "cast", "director", "listed_in"), 2):
             print(f"{table1}_per_{table2}")
             cur.execute(f"DROP VIEW IF EXISTS [{table1}_per_{table2}]")
             cur.execute(f"""
@@ -138,6 +138,10 @@ class db_connector:
                 FROM [{table1}_per_show] AS t1
                 LEFT JOIN [{table2}_per_show] AS t2
                 ON t1.show_id = t2.show_id
+                WHERE t1.name NOT NULL
+                AND t1.name != ""
+                AND t2.name NOT NULL
+                AND t2.name != ""
                 """)
 
         self.con.commit()
@@ -149,195 +153,144 @@ class db_connector:
         data.set_index("show_id", inplace=True)
         data.to_csv(filename, encoding="UTF-8")
 
-    def __get_all(self, str: )
+    def __get_all(self, table: str):
+        cur = self.con.cursor()
+        cur.execute(f"""
+            SELECT *
+            FROM [{table}]
+            """)
+        return pd.DataFrame(cur.fetchall())
+
+    def __get_filtered(self, table: str, filter_table: str, filter_item: str):
+        cur = self.con.cursor()
+        cur.execute(f"""
+            SELECT *
+            FROM [{table}_per_{filter_table}]
+            WHERE {filter_table} = ?
+            """,
+            (filter_item,))
+        return pd.DataFrame(cur.fetchall())
 
 # Welche Schauspieler gibt es bei Netflix-Filmen?
     def get_all_cast(self):
-        cur = self.con.cursor()
-        cur.execute("""SELECT c.name
-                    FROM [cast] as c
-                    """)
-        return pd.DataFrame(cur.fetchall(), columns=("cast",))
+        data = self.__get_all("cast")
+        data.columns = ("id", "cast")
+        return data
 
 # Welche Kategorien gibt es auf Netflix?
-    def get_all_listings(self):
-        cur = self.con.cursor()
-        cur.execute("""SELECT l.name
-                    FROM [listing] as l
-                    """)
-        return pd.DataFrame(cur.fetchall(), columns=("listed_in",))
+    def get_all_listed_in(self):
+        data = self.__get_all("listed_in")
+        data.columns = ("id", "listed_in")
+        return data
 
 # Welche Regisseure gibt es bei Netflix-Filmen?
     def get_all_directors(self):
-        cur = self.con.cursor()
-        cur.execute("""
-                    SELECT d.name
-                    FROM [director] as d
-                    """)
-        return pd.DataFrame(cur.fetchall(), columns=("director",))
+        data = self.__get_all("director")
+        data.columns = ("id", "director")
+        return data
 
 # Welche Filme gibt es auf Netflix?
     def get_all_shows(self):
-        cur = self.con.cursor()
-        cur.execute(f"""
-                    SELECT {', '.join(self.SHOW_COLUMNS)}
-                    FROM [shows]
-                    """)
-        return pd.DataFrame(cur.fetchall(), columns=self.SHOW_COLUMNS)
+        data = self.__get_all("show")
+        data.columns = self.SHOW_COLUMNS
+        return data
 
 # In welchen Ländern wurden Filme produziert?
     def get_all_countries(self):
-        cur = self.con.cursor()
-        cur.execute("""
-                    SELECT c.name
-                    FROM [country] as c
-                    """)
-        return pd.DataFrame(cur.fetchall(), columns=("country",))
+        data = self.__get_all("country")
+        data.columns=(id, "country")
+        return data
 
 # Welche Filme sind vom Regisseur X?
     def get_shows_by_director(self, director: str):
-        cur = self.con.cursor()
-        cur.execute(f"""
-                    SELECT s.{", s.".join(self.SHOW_COLUMNS)}
-                    FROM [shows] as s, [director] as d, [show_director] as sd
-                    WHERE s.show_id = sd.show_id
-                    AND d.id = sd.director_id
-                    AND d.name = ?
-                    """,
-                    (director,))
-        return pd.DataFrame(cur.fetchall(), columns=self.SHOW_COLUMNS)
+        data = self.__get_filtered("show", "director", director)
+        data.columns = self.SHOW_COLUMNS + ["director"]
+        return data
 
 # In welchen Filmen hat Schauspieler X gespielt?
     def get_shows_by_cast(self, cast: str):
-        cur = self.con.cursor()
-        cur.execute(f"""
-                    SELECT s.{", s.".join(self.SHOW_COLUMNS)}
-                    FROM [shows] as s, [cast] as c, [show_cast] as sc
-                    WHERE s.show_id = sc.show_id
-                    AND c.id = sc.cast_id
-                    AND c.name = ?
-                    """,
-                    (cast,))
-        return pd.DataFrame(cur.fetchall(), columns=self.SHOW_COLUMNS)
+        data = self.__get_filtered("show", "cast", cast)
+        data.columns = self.SHOW_COLUMNS + ["cast"]
+        return data
 
 # Welche Filme sind in der Kategorie X?
-    def get_shows_by_listing(self, listing: str):
-        cur = self.con.cursor()
-        cur.execute(f"""
-                    SELECT s.{", s.".join(self.SHOW_COLUMNS)}
-                    FROM [shows] as s, [listing] as l, [show_listing] as sl
-                    WHERE s.show_id = sl.show_id
-                    AND l.id = sl.listing_id
-                    AND l.name = ?
-                    """,
-                    (listing,))
-        return pd.DataFrame(cur.fetchall(), columns=self.SHOW_COLUMNS)
+    def get_shows_by_listed_in(self, listed_in: str):
+        data = self.__get_filtered("show", "listed_in", listed_in)
+        data.columns = self.SHOW_COLUMNS + ["listed_in"]
+        return data
 
 # Welche Filme sind aus Land X?
     def get_shows_by_country(self, country: str):
-        cur = self.con.cursor()
-        cur.execute(f"""
-                    SELECT s.{", s.".join(self.SHOW_COLUMNS)}
-                    FROM [shows] as s, [country] as c, [show_country] as sc
-                    WHERE s.show_id = sc.show_id
-                    AND c.id = sc.country_id
-                    AND c.name = ?
-                    """,
-                    (country,))
-        return pd.DataFrame(cur.fetchall(), columns=self.SHOW_COLUMNS)
+        data = self.__get_filtered("show", "country", country)
+        data.columns = self.SHOW_COLUMNS + ["country"]
+        return data
 
 # 5. Wie viele Filme gibt es pro Land?
     def get_shows_per_country(self):
         cur = self.con.cursor()
         cur.execute("""
-                    SELECT c.name, COUNT(c.name)
-                    FROM [country] as c, [show_country] as sc
-                    WHERE c.id = sc.country_id
-                    GROUP BY c.name
-                    """)
-        return pd.DataFrame(cur.fetchall(), columns=["country", "count"])
+            SELECT name, count(name) AS count
+            FROM [country_per_show]
+            GROUP BY name
+            """)
+        return pd.DataFrame(cur.fetchall(),
+                            columns=("name", "count"))
 
 #  An welchen Orten wurde Film X gedreht?
     def get_countries_by_show(self, show: str):
-        cur = self.con.cursor()
-        cur.execute("""
-                    SELECT c.name
-                    FROM [shows] as s, [country] as c, [show_country] as sc
-                    WHERE s.show_id = sc.show_id
-                    AND c.id = sc.country_id
-                    AND s.title = ?
-                    """,
-                    (show,))
-        return pd.DataFrame(cur.fetchall(), columns=("country",))
-
+        data = self.__get_all("show_per_country")
+        data.columns = self.SHOW_COLUMNS + ["country"]
+        return data[data["title"]==show]
+        
+# TODO: Kommentar sollte anders rum sein
 #1. In welchen Filmen war X ein Regisseur?
     def get_directors_by_show(self, show: str):
-        cur = self.con.cursor()
-        cur.execute("""
-                    SELECT d.name
-                    FROM [shows] as s, [director] as d, [show_director] as sd
-                    WHERE s.show_id = sd.show_id
-                    AND d.id = sd.director_id
-                    AND s.title = ?
-                    """,
-                    (show,))
-        return pd.DataFrame(cur.fetchall(), columns=("director",))
+        data = self.__get_all("show_per_director")
+        data.columns = self.SHOW_COLUMNS + ["director"]
+        return data[data["show"]==show]
 
+# TODO: Auch hier sollte es anders rum sein
 # In welchen Filmen war X ein Schauspieler?
     def get_cast_by_show(self, show: str):
-        cur = self.con.cursor()
-        cur.execute("""
-                    SELECT c.name
-                    FROM [shows] as s, [cast] as c, [show_cast] as sc
-                    WHERE s.show_id = sc.show_id
-                    AND c.id = sc.cast_id
-                    AND s.title = ?
-                    """,
-                    (show,))
-        return pd.DataFrame(cur.fetchall(), columns=("cast",))
+        data = self.__get_all("show_per_cast")
+        data.columns = self.SHOW_COLUMNS + ["cast"]
+        return data[data["show"]==show]
 
 # In welchen Kategorien ist Film X?
-    def get_listings_by_show(self, show: str):
-        cur = self.con.cursor()
-        cur.execute("""
-                    SELECT l.name
-                    FROM [shows] as s, [listing] as l, [show_listing] as sl
-                    WHERE s.show_id = sl.show_id
-                    AND l.id = sl.listing_id
-                    AND s.title = ?
-                    """,
-                    (show,))
-        return pd.DataFrame(cur.fetchall(), columns=("listings",))
+    def get_listed_ins_by_show(self, show: str):
+        data = self.__get_all("show_per_listed_in")
+        data.columns = self.SHOW_COLUMNS + ["listed_in"]
+        return data[data["show"]==show]
 
 # In welchen Kategorien ist X ein Regisseur? 
-    def get_listings_by_director(self, director: str):
+    def get_listed_ins_by_director(self, director: str):
         cur = self.con.cursor()
         cur.execute("""
                     SELECT DISTINCT l.name
-                    FROM [listing] as l, [show_listing] as sl,
+                    FROM [listed_in] as l, [show_listed_in] as sl,
                          [director] as d, [show_director] as sd
                     WHERE sl.show_id = sd.show_id
-                    AND l.id = sl.listing_id
+                    AND l.id = sl.listed_in_id
                     AND d.id = sd.director_id
                     AND d.name = ?
                     """,
                     (director,))
-        return pd.DataFrame(cur.fetchall(), columns=("listing",))
+        return pd.DataFrame(cur.fetchall(), columns=("listed_in",))
 
 # In welchen Kategorien ist X ein Schauspieler?
-    def get_listings_by_cast(self, cast: str):
+    def get_listed_ins_by_cast(self, cast: str):
         cur = self.con.cursor()
         cur.execute("""
                     SELECT DISTINCT l.name
-                    FROM [listing] as l, [show_listing] as sl,
+                    FROM [listed_in] as l, [show_listed_in] as sl,
                          [cast] as c, [show_cast] as sc
                     WHERE sl.show_id = sc.show_id
-                    AND l.id = sl.listing_id
+                    AND l.id = sl.listed_in_id
                     AND c.id = sc.cast_id
                     AND c.name = ?
                     """,
                     (cast,))
-        return pd.DataFrame(cur.fetchall(), columns=("listing",))
+        return pd.DataFrame(cur.fetchall(), columns=("listed_in",))
 
 # Welcher Schauspieler spielte bei Filmen des Regisseures X mit?
     def get_cast_by_director(self, director: str):
@@ -414,22 +367,31 @@ class db_connector:
                     (cast,))
         return pd.DataFrame(cur.fetchall(), columns=("country",))
 
+    def get_type_count(self):
+        cur = self.con.cursor()
+        cur.execute("""
+                SELECT type, count(type)
+                FROM show
+                """)
+        return pd.DataFrame(cur.fetchall(),
+                            columns=("type", "count"))
+
     def get_full_table(self):
         # TODO: Fertig machen
         cur = self.con.cursor()
         cur.execute("""
                     SELECT s.show_id, s.type, s.title, group_concat(DISTINCT director.name) AS director, group_concat(DISTINCT casts.name) AS cast,
                         group_concat(DISTINCT country.name) AS country, s.date_added, s.release_year, s.rating,
-                        s.duration, group_concat(DISTINCT listing.name) AS "listed in", s.description
-                    FROM [shows] as s
+                        s.duration, group_concat(DISTINCT listed_in.name) AS "listed in", s.description
+                    FROM [show] as s
                     LEFT JOIN [cast_per_show] as casts
                         ON s.show_id = casts.show_id
                     LEFT JOIN [country_per_show] as country
                         ON s.show_id = country.show_id
                     LEFT JOIN [director_per_show] as director
                         ON s.show_id = director.show_id
-                    LEFT JOIN [listing_per_show] as listing
-                        ON s.show_id = listing.show_id
+                    LEFT JOIN [listed_in_per_show] as listed_in
+                        ON s.show_id = listed_in.show_id
                     GROUP BY s.show_id, s.type, s.title, s.date_added,
                         s.release_year, s.rating, s.duration, s.description
                     """)
@@ -470,7 +432,11 @@ class db_connector:
 
 if __name__ == "__main__":
     con = db_connector()
-    con.reset_database()
-    con.import_file("netflix_titles.csv")
-    con.export_csv()
+    #con.reset_database()
+    #con.import_file("netflix_titles.csv")
+    #con.export_csv()
+    test = con.get_countries_by_show("Houston, We Have a Problem!")
+    #test = con._db_connector__get_all("show_per_country")
+    #test.columns = con.SHOW_COLUMNS + ["country"]
+    #test = test[test["title"]=="Houston, We Have a Problem!"]
     con.con.close()
